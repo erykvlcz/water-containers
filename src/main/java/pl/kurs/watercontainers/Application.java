9db9614eec70c62661ec6bf8e437e7ded632d50e
Eryk Kowalczyk <kowalczykeryk@op.pl>
2025-04-16 19:18:03 +0200

    Added WaterContainer model

---
+++ src/main/java/pl/kurs/watercontainers/Application.java
@@ -0,0 +1,11 @@
+package pl.kurs.watercontainers;
+
+import pl.kurs.watercontainers.models.WaterContainer;
+
+public class Application {
+    public static void main(String[] args) {
+        WaterContainer beczka = new WaterContainer("Beczka", 300, 100);
+
+
+    }
+}
---
+++ src/main/java/pl/kurs/watercontainers/models/WaterContainer.java
@@ -0,0 +1,37 @@
+package pl.kurs.watercontainers.models;
+
+import java.util.Objects;
+
+public class WaterContainer {
+    private String name;
+    private double maxCapacity;
+    private double waterLevel;
+
+    public WaterContainer(String name, double maxCapacity, double waterLevel) {
+        this.name = name;
+        this.maxCapacity = maxCapacity;
+        this.waterLevel = waterLevel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+        WaterContainer that = (WaterContainer) o;
+        return Double.compare(maxCapacity, that.maxCapacity) == 0 && Double.compare(waterLevel, that.waterLevel) == 0 && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, maxCapacity, waterLevel);
+    }
+
+    @Override
+    public String toString() {
+        return "WaterContainer{" +
+                "name='" + name + '\'' +
+                ", maxCapacity=" + maxCapacity +
+                ", waterLevel=" + waterLevel +
+                '}';
+    }
+}
