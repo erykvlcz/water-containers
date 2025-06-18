b36889c9ba2fd9943894126e312bb635efd1ea90
Michal Sloniewski <michal.sloniewski@smartcodecreations.com>
2025-06-18 19:26:18 +0200

    added Car model

---
+++ src/main/java/pl/kurs/watercontainers/models/Car.java
@@ -0,0 +1,17 @@
+package pl.kurs.watercontainers.models;
+
+public class Car {
+
+
+    private String producer;
+    private String model;
+    private int powerHp;
+
+    public Car(String producer, String model, int powerHp) {
+        this.producer = producer;
+        this.model = model;
+        this.powerHp = powerHp;
+    }
+
+
+}
